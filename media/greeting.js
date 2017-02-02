eb1bf09ea0eb934b1a3e6e8f9149a6d0e795d38e
Simone Gonzalez <simone.b.gonzalez@gmail.com>
2017-02-01 19:57:50 -0600

    add js to media page

---
+++ media/greeting.js
@@ -0,0 +1,22 @@
+// var name = prompt("Hi there! What's your name?");
+// var output = document.querySelector('#greeting');
+//
+// output.innerHTML = "<p>Thanks for visiting, " + name + ".</p>";
+//
+
+//user clicks element x
+var btn = document.querySelector('#userButton');
+btn.addEventListener('click', function(event) {
+  alert("Hey, don't do that.")
+});
+/*Tonight */
+var richardFood = "sushi";
+var simoneFood = "Pho";
+var genaFood = "pasta";
+
+var myFoodArray = ["Tacos", "Burriotos", richardFood, "curry", simoneFood, genaFood];
+
+var classCity =
+{
+  "gena": "Ashland", "rebecca": "Memphis", "wayne": "Manchester", "richard": "Galveston", "simone": "Louisville"
+}
