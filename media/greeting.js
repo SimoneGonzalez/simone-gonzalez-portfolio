3a0d6ce673171095b18f191a8f720fff5236e41b
Simone Gonzalez <simone.b.gonzalez@gmail.com>
2017-02-02 18:41:25 -0600

    edit greeter

--- media/greeting.js
+++ media/greeting.js
@@ -1,15 +1,24 @@
-// var name = prompt("Hi there! What's your name?");
-// var output = document.querySelector('#greeting');
+
+var firstName = prompt("Hi there! What's your first name?") || "Visitor";
+var lastName = prompt("What's your last name?") || "McDefaultson";
+
+var output = document.querySelector('#greeting');
+
+output.innerHTML = "<p>Thanks for visiting, " + firstName + " " + lastName + ".</p>";
 //
-// output.innerHTML = "<p>Thanks for visiting, " + name + ".</p>";
-//
+// if(firstName && lastName) {
+//   output.innerHTML = "<p>Thanks for visiting, " + firstName + " " + lastName + ".</p>";
+// } else {
+//   output.innerHTML = "<p>Please tell us your name!</p>";
+// }
+
 
 //user clicks element x
 var btn = document.querySelector('#userButton');
 btn.addEventListener('click', function(event) {
   alert("Hey, don't do that.")
 });
-/*Tonight */
+
 var richardFood = "sushi";
 var simoneFood = "Pho";
 var genaFood = "pasta";
