2e39411ec40f843ee1dd6bbb73e4a13d8896c90f
Simone Gonzalez <simone.b.gonzalez@gmail.com>
2017-02-16 19:59:14 -0600

    add javascript file to portfolio

---
+++ main.js
@@ -0,0 +1,36 @@
+/********Add a click handler for your img.
+When users click on your photo on your profile page, a previously-hidden blurb should be revealed with a jQuery animation.
+*********/
+
+
+
+
+
+
+
+
+// $('li').on('mouseover', function() {
+//   $('li').addClass('highlighted');
+// });
+//
+// $('li').on('mouseleave', function() {
+//   $('li').removeClass('highlighted');
+// });
+//
+// $('li').on('mouseover', function(event){
+//   var target = event.target;
+//   $(target).addClass('highlighted');
+// });
+//
+// $('li').on('mouseleave', function(event){
+//   var target = event.target;
+//   $(target).removeClass('highlighted')
+// });
+
+// $('li').on('mouseover', function(){
+//   $(this).addClass('highlighted');
+// });
+//
+// $('li').on('mouseleave', function() {
+//   $(this).removeClass('highlighted');
+// });
